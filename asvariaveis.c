4a4e9a26358c620987ab6175caa149efcf780508
informarcos2908-glitch <informarcos2908@gmail.com>
2026-02-26 01:00:18 +0000

    Nessa parte coloquei explicações sobre os especificadores nos cometários

--- asvariaveis.c
+++ asvariaveis.c
@@ -2,7 +2,12 @@
 
   int main(){
 
-    printf("Olá Mundo! \n");
+      //Sitaxe padrão printf
+      printf("Olá Mundo!");
+      //printf("texto com formatação", variável1, variável2);
+      //printf("Olá MUndo!\n"); o contra barra+n é para pular linha
+      
+
   return 0;
 
   }
